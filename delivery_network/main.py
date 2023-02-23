0adf126d14ae287dc0183c90e04d60282bf5cb60
hcapot <hugo.capot@ensae.fr>
2023-02-23 13:25:53 +0000

    Version 23/02/2023

--- delivery_network/main.py
+++ delivery_network/main.py
@@ -1,8 +1,13 @@
-from graph import Graph, graph_from_file
+from graph import Graph, graph_from_file 
 
 
-data_path = "input/"
-file_name = "network.01.in"
+data_path = "input/" 
+file_name = "network.03.in" 
 
-g = graph_from_file(data_path + file_name)
-print(g)
+g = graph_from_file(data_path + file_name) 
+print(g) 
+
+
+#g=Graph([]) 
+#g.add_edge("Paris","Palaiseau",4,20) 
+#print(g) 
